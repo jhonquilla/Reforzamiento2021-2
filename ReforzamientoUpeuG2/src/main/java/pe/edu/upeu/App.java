b8338563cb6d6a8d16c35dfdef3a27ae758ce58f
jhonquilla <jhondiery@hotmail.comcd>
2021-10-24 17:56:10 +0000

    prueba2

---
+++ ReforzamientoUpeuG2/src/main/java/pe/edu/upeu/App.java
@@ -0,0 +1,15 @@
+package pe.edu.upeu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Basicojava bj=new Basicojava();
+        bj.tablaMultiplicar();
+    }
+}
---
+++ ReforzamientoUpeuG2/src/main/java/pe/edu/upeu/Basicojava.java
@@ -0,0 +1,28 @@
+package pe.edu.upeu;
+
+public class Basicojava {
+
+public void tablaMultiplicar() {
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 10; j++) {
+           System.out.println(i+" x "+j+" = "+(i*j)); 
+        }
+        System.out.println(" ");
+    }
+    
+}
+
+
+public void numerosprimos() {
+    for (int i = 1; i <=20; i++) {
+        for (int j = 1; j <20; j++) {
+            System.out.println(i+" / "+j+" = "+(i/j));
+            
+        }
+    }
+    
+}
+
+}
+
+
